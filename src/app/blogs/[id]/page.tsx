1309e5874b02c26111674a21c748e1d528f8135b
Fortress Michael <kingfortressidebuemi@gmail.com>
2025-10-17 10:43:19 -0800

    website update

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // @ts-nocheck
+
 "use server";
 
 import Navbar from "@/components/Navbar";
