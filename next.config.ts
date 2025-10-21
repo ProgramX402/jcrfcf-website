dfc1c3a1abc266e84771abb24f8b746df8b6e614
Fortress Michael <kingfortressidebuemi@gmail.com>
2025-10-21 10:57:36 -0800

    dependency issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', "images.unsplash.com", "images.pexels.com"], // allow Cloudinary images
+    domains: ['res.cloudinary.com', 'images.unsplash.com', 'images.pexels.com'], // allow Cloudinary images
   },
 };
 
