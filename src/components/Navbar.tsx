8b9025dfa3a07dd68e4b4fe0696298bb95255b3d
Fortress Michael <kingfortressidebuemi@gmail.com>
2025-10-28 13:32:08 -0800

    handled navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
+    <header className="bg-white shadow-md fixed top-0 w-full z-50">
       <nav className="container mx-auto flex justify-between items-center py-4 px-6">
         {/* Logo */}
         <Link href="/" className="flex items-center space-x-2">
